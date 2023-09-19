3bf33eb5f4782ce2d29c9ffe5683e6e07d7b9eb3
Mike Roest <msroest@gmail.com>
2023-09-19 12:19:47 -0600

    add scripts and readme update

--- imagewrite.py
+++ imagewrite.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python3
+#!./.venv/bin/python3
 # -*- coding:utf-8 -*-
 import logging
 import sys
-from epaper import epd7in5_V2
+import epaper
 from PIL import Image
-import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    logging.info("epd7in5_V2 Magic mirror write")
-    epd = epd7in5_V2.EPD()
+    logging.info("Magic mirror write")
+    epd = epaper.epaper(sys.argv[1]).EPD()
     logging.info("init and Clear")
     epd.init()
     epd.Clear()
-    Himage = Image.open(sys.argv[1])
+    Himage = Image.open(sys.argv[2])
     epd.display(epd.getbuffer(Himage))
 except IOError as e:
     logging.info(e)
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MAGICMIRROR_HOST=localhost
+MAGICMIRROR_PORT=8080
+DISPLAY_WIDTH=800
+DISPLAY_HEIGHT=480
+DISPLAY_MODEL=epd7in5_V2
+LOAD_DELAY=10
+
+if [ -f ./config.sh ]; then
+   source ./config.sh
+fi
+docker pull msroest/rpi-magicmirror-eink:latest
+docker run -it --rm -e MAGICMIRROR_HOST=$MAGICMIRROR_HOST -e MAGICMIRROR_PORT=$MAGICMIRROR_PORT -e DISPLAY_WIDTH=$DISPLAY_WIDTH -e DISPLAY_HEIGHT=$DISPLAY_HEIGHT -e LOAD_DELAY=$LOAD_DELAY -v ./out:/opt/app/out msroest/rpi-magicmirror-eink:latest
+$(dirname "$0")/imagewrite.sh $DISPLAY_MODEL out/mm.png
