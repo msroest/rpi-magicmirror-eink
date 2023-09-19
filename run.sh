a0173a76be6b22d44e1d66dc57282ddd9d7a04d0
Mike Roest <msroest@gmail.com>
2023-09-19 14:23:19 -0600

    remove docker

--- run.sh
+++ run.sh
@@ -9,6 +9,5 @@ LOAD_DELAY=10
 if [ -f $(dirname "$0")/config.sh ]; then
    source $(dirname "$0")/config.sh
 fi
-docker pull msroest/rpi-magicmirror-eink:latest
-docker run -it --rm -e MAGICMIRROR_HOST=$MAGICMIRROR_HOST -e MAGICMIRROR_PORT=$MAGICMIRROR_PORT -e DISPLAY_WIDTH=$DISPLAY_WIDTH -e DISPLAY_HEIGHT=$DISPLAY_HEIGHT -e LOAD_DELAY=$LOAD_DELAY -v /etc/localtime:/etc/localtime:ro -e TZ=`date +%Z` -v $(dirname "$0")/out:/opt/app/out msroest/rpi-magicmirror-eink:latest
+npm start
 $(dirname "$0")/.venv/bin/python3 $(dirname "$0")/imagewrite.py $DISPLAY_MODEL $(dirname "$0")/out/mm.png
