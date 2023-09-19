9672e1235592b8de6dd3dec6ba3737fe330a1493
Mike Roest <msroest@gmail.com>
2023-09-19 14:27:38 -0600

    try again

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ DISPLAY_WIDTH=800
 DISPLAY_HEIGHT=480
 DISPLAY_MODEL=epd7in5_V2
 LOAD_DELAY=10
+CHROMIUM_PATH=/usr/bin/chromium
 
 if [ -f $(dirname "$0")/config.sh ]; then
    source $(dirname "$0")/config.sh
 fi
 pushd $(dirname "$0")
-npm start
+MAGICMIRROR_HOST=$MAGICMIRROR_HOST MAGICMIRROR_PORT=$MAGICMIRROR_PORT DISPLAY_WIDTH=$DISPLAY_WIDTH DISPLAY_HEIGHT=$DISPLAY_HEIGHT LOAD_DELAY=$LOAD_DELAY $CHROMIUM_PATH=CHROMIUM_PATH npm start
 $(dirname "$0")/.venv/bin/python3 $(dirname "$0")/imagewrite.py $DISPLAY_MODEL $(dirname "$0")/out/mm.png
 popd
